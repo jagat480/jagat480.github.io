00bf735b693ccaaa2120f56ab95302c0a8ba02f8
smile <your.2023ucp1623@mnit.ac.in>
2024-10-17 12:36:39 +0530

    changes in main branch

---
+++ file.py
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+
+# Python Program illustrating
+# dot product of two vectors
+
+=======
+#branch parallel
+ 
+>>>>>>> parallel
+# Importing numpy module
+import numpy as np
+
+# Taking two 1D array
+a = 3 + 1j
+b = 7 + 6j
+
+# Calculating dot product using dot()
+print(np.dot(a, b))
+
+
